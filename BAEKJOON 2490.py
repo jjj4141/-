10a2edfe053d1f8b62bda82db89bd8d15cdedeb9
jjj4141 <wodl6942@gmail.com>
2026-03-19 12:23:39 +0900

    Create BAEKJOON 2490.py

---
+++ BAEKJOON 2490.py
@@ -0,0 +1,9 @@
+f = input().split()
+s = input().split()
+t = input().split()
+
+result = ["E", "A", "B", "C", "D"]
+
+print(result[f.count('0')])
+print(result[s.count('0')])
+print(result[t.count('0')])
