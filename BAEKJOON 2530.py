9f995138a236acd9a9325e2946d421dade35abf0
jjj4141 <wodl6942@gmail.com>
2026-03-19 11:32:12 +0900

    Update and rename BAEKJOON.py to BAEKJOON 2530.py

---
+++ BAEKJOON 2530.py
@@ -0,0 +1,17 @@
+h, m, s = map(int, input().split())
+a = int(input())
+
+s += a
+
+if(s >= 60):
+    m += s // 60
+    s = s % 60
+
+if(m >= 60):
+    h += m // 60    
+    m = m % 60
+
+if(h >= 24):
+    h = h % 24
+
+print(h, m, s)
