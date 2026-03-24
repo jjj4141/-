1324e5c24d1d6b7a80e3672640058c8deef6271e
jjj4141 <wodl6942@gmail.com>
2026-03-24 13:32:16 +0900

    Create BAEKJOON 9498.py

---
+++ BAEKJOON 9498.py
@@ -0,0 +1,16 @@
+a = int(input())
+
+if(a <= 100 and a >= 90):
+    print("A")
+
+if(a >= 80 and a <= 89):
+    print("B")
+
+if(a >= 70 and a <= 79):
+    print("C")
+
+if(a >= 60 and a <= 69):
+    print("D")
+
+if(a <= 59):
+    print("F")
