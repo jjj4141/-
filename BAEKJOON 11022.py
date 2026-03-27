8b86c2a808b1f3a272571c145b4c928c701e2fa2
jjj4141 <wodl6942@gmail.com>
2026-03-27 12:02:19 +0900

    Create BAEKJOON 11022.py

---
+++ BAEKJOON 11022.py
@@ -0,0 +1,7 @@
+testcase = int(input())
+casenumber = 1
+
+for i in range(testcase):
+    a, b = map(int, input().split())
+    print("Case #", casenumber, ": ", a, " + ", b, " = ", a + b, sep="")
+    casenumber +=1
