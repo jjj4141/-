92a39b55d4e8f20d47117e65c33fc3d5fab7ffea
jjj4141 <wodl6942@gmail.com>
2026-04-16 13:31:11 +0900

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,56 @@
+import copy
+
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row-1][i] == num:
+            return False
+        
+    for i in range(9):
+        if board[i][col-1] == num:
+            return False
+        
+    row = (row-1)//3 * 3
+    col = (col-1)//3 * 3
+    
+    for i in range(row, row + 3):
+        for u in range(col, col + 3):
+            if board[i][u] == num:
+                return False
+            
+    return True
+
+def solve_sudoku(board):
+    tempBoard = copy.deepcopy(board)
+    for i in range(1, 10):
+        for u in range(1, 10):
+            if board[i-1][u-1] == 0:
+                for n in range(1, 10):
+                    if is_valid(board, i, u, n):
+                        tempBoard[i-1][u-1] = n
+                        solve_sudoku(tempBoard)
+    for i in range(1, 10):
+        for u in range(1, 10):
+            if tempBoard[i-1][u-1] == 0:
+                return False
+    board = tempBoard
+    return True
+
+def print_board(board):
+    print(board)
+
+puzzle = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0], 
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+if solve_sudoku(puzzle):
+    print(print_board(puzzle))
+else:
+    print("해답을 찾을 수 없습니다.")
